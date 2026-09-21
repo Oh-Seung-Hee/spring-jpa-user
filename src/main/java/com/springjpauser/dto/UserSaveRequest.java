2d108e452a9b7c1e2652f30bf27eb3230e70103c
OH <dhtmdgml@knou.ac.kr>
2026-09-21 11:44:34 +0900

    feat : 예외처리 - Bean Validation

--- src/main/java/com/springjpauser/dto/UserSaveRequest.java
+++ src/main/java/com/springjpauser/dto/UserSaveRequest.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 public class UserSaveRequest {
 
-    @Size(min = 4)
+    @Size(min = 4, message = "name은 4글자 이상이어야합니다.")
     private String name;
     private String email;
     private String address;
